bf8a04535934883be9f83179a5ccd146154447c0
Robin Levavasseur <robin.levavasseur@epitech.eu>
2021-06-13 20:09:50 +0200

    feat Button: setup cmakelist and directory/files

---
+++ src/Object/Button/Button.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2021
+** Indie_Studio
+** File description:
+** Button
+*/
+
+#ifndef BUTTON_HPP_
+#define BUTTON_HPP_
+
+#include "AObject.hpp"
+
+class Button {
+    public:
+        Button();
+        ~Button();
+
+    protected:
+    private:
+};
+
+#endif /* !BUTTON_HPP_ */
