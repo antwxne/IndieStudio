bca8e6b0d13ee6b4e1556efdf515853403af84b3
Antoine DESRUET <antoine.desruet@epitech.eu>
2021-06-16 16:25:02 +0200

    edit: update particle
    reset particle pos to object
    reset velocity and set a new random acceleration

--- src/Object/Particles.cpp
+++ src/Object/Particles.cpp
@@ -10,22 +10,29 @@
 
 #include "Particles.hpp"
 
-Particles::Particles(const coords &pos, const std::pair<int, int> &size, const std::pair<int, int> &maxSize,
-    float scale, const std::pair<RGB, RGB> &colors, std::size_t nParticles
-) : AObject(pos, size, scale, colors),_particles(), _maxSize(maxSize), _objPos(pos)
+Particles::Particles(const coords &pos, const std::pair<int, int> &size, float maxSize,
+    float scale, const std::pair<RGB, RGB> &colors, std::size_t nParticles, const coords &accelleration
+) : AObject(pos, size, scale, colors),_particles(), _maxSize(maxSize), _objPos(pos), _acceleration(accelleration)
 {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> alpha(100, 250);
-    std::uniform_real_distribution<float> acc(-0.5f, 0.5f);
+    std::uniform_real_distribution<float> acc(0, 0.5f);
     std::uniform_real_distribution<float> vel(-20, 20);
-    particle tmp {.position = pos, .color = colors.first, .size = size, .scale = scale};
+    particle tmp {.position = pos, .color = colors.first, .radius = static_cast<float>(size.first), .scale = scale};
+    float tmpRand;
 
     _particles.reserve(nParticles);
     _typeField.isParticle = true;
     for (std::size_t i = 0; i < nParticles; ++i) {
         tmp.color.a = alpha(gen);
-        tmp.a = {acc(gen), acc(gen), acc(gen)};
+        tmp.a = accelleration;
+        tmpRand = acc(gen);
+        tmp.a.first += tmp.a.first > 0 ? tmpRand : -tmpRand;
+        tmpRand = acc(gen);
+        tmp.a.second += tmp.a.second > 0 ? tmpRand : -tmpRand;
+        tmpRand = acc(gen);
+        tmp.a.third += tmp.a.third > 0 ? tmpRand : -tmpRand;
         tmp.v = {vel(gen), vel(gen), vel(gen)};
         _particles[i] = tmp;
     }
@@ -34,21 +41,31 @@ const std::vector<Particles::particle> &Particles::getParticles() const noexcept
 {
     return _particles;
 }
-void Particles::update(const coords &objPos) noexcept
+void Particles::update() noexcept
 {
     std::random_device rd;
     std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> vel(-20, 20);
+    std::uniform_real_distribution<float> acc(0, 0.5f);
     bool r;
+    float tmpRand;
 
     for (auto &i : _particles) {
         r = gen() % 10 <= 3;
         i.v += i.a;
         i.position += i.v;
-        i.size.first += r;
-        i.size.second += r;
-        if (i.size.first <= _maxSize.first || i.size.second <= _maxSize.second) {
-            i.position = objPos;
-            i.size = _size;
+        i.radius += static_cast<float>(r);
+        if (i.radius > _maxSize) {
+            i.position = _objPos;
+            i.radius = _size.first;
+            i.v = {vel(gen), vel(gen), vel(gen)};
+            i.a = _acceleration;
+            tmpRand = acc(gen);
+            i.a.first += i.a.first > 0 ? tmpRand : -tmpRand;
+            tmpRand = acc(gen);
+            i.a.second += i.a.second > 0 ? tmpRand : -tmpRand;
+            tmpRand = acc(gen);
+            i.a.second += i.a.second > 0 ? tmpRand : -tmpRand;
         }
     }
 
@@ -63,4 +80,8 @@ void Particles::changeColor(const RGB &color) noexcept
         i.color = color;
         i.color.a = 100 + (dist(gen) % 155);
     }
+}
+void Particles::setAcceleration(const coords &acceleration)
+{
+    _acceleration = acceleration;
 }
