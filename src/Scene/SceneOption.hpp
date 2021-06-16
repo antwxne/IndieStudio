78f114933ff7e983fa284ffeae4b3ca068780172
Charlie CHOU <charlie.chou@epitech.eu>
2021-06-16 12:16:29 +0200

    add: UiScenes for better implementation of UiScenes

--- src/Scene/SceneOption.hpp
+++ src/Scene/SceneOption.hpp
@@ -9,17 +9,30 @@
 #define SCENEOPTION_HPP_
 
 #include "Raylib.hpp"
-#include "AScene.hpp"
+#include "UiScene.hpp"
 
-class SceneOption : public AScene
-{
-    public:
-        SceneOption(Setting &settings);
-        ~SceneOption();
-        Scenes run(Raylib &lib, Scenes const &prevScene) final;
-    
-    private:
-        bool _quit;
-};
+namespace option {
+
+    static const std::string _returnText = "Return";
+
+    static const struct coords _menuPos = coords(200.0f, 900.0f, 0.0f);
+
+    static const std::string _bgPath = "asset/background_asset/Background_02.png";
+
+    class SceneOption : public UiScene
+    {
+        public:
+            SceneOption(Setting &settings);
+            ~SceneOption();
+            void eventScene(Raylib &lib) final;
+            Scenes endScene(Scenes const &prevScene) noexcept final;
+
+        private:
+            std::pair<float, float> _mousePos;
+            bool _quit;
+            bool _clicked;
+    };
+
+}
 
 #endif /* !SCENEOPTION_HPP_ */
