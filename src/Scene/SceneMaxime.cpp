5a823ad95625d33ec6fb9780cdb7c2008e2267f1
Maxime <maxime.dodin@epitech.eu>
2021-06-17 22:13:24 +0200

    fix: indentation in file

--- src/Scene/SceneMaxime.cpp
+++ src/Scene/SceneMaxime.cpp
@@ -25,44 +25,44 @@
 #include "Object/Collisionable/Wall/Wall.hpp"
 #include "Object/Collisionable/Destructible/Movable/PowerUps/PowerUps.hpp"
 
-const std::vector<std::string> SceneMaxime::_assetsPath {
+const std::vector<std::string> SceneMaxime::_assetsPath{
     "asset/background_asset/ground.png",
     "asset/OBJFormat/ground.obj",
-    "asset/bonus/arrow.obj"
-};
+    "asset/bonus/arrow.obj"};
 
 SceneMaxime::SceneMaxime(Setting &settings) : AScene(settings), _pressed(false)
 {
-/////////////////////////////START CEMENT//////////////////////:
+    /////////////////////////////START CEMENT//////////////////////:
 
-    _objects.emplace_back(std::make_shared<Ground>(coords(0, 0, 0), std::make_pair(40, 22), std::pair<std::string, std::string>(_assetsPath.at(0), _assetsPath.at(1))));
+    _objects.emplace_back(std::make_shared<Ground>(
+        coords(0, 0, 0), std::make_pair(40, 22), std::pair<std::string, std::string>(_assetsPath.at(0), _assetsPath.at(1))));
     auto const &carre = std::make_unique<FrameUI>();
     for (auto const &carr : carre->getBorder())
         _objects.emplace_back(std::make_shared<BorderPlayer>(carr));
-    for (unsigned int i = 0; i != _posTank.size(); i++) {
+    for (unsigned int i = 0; i != _posTank.size(); i++)
+    {
         auto tmp = _objects.emplace_back(
-            std::make_shared<Tank>(_settings._players.at(i).name,
-                coords(_posTank[i].first, 0, _posTank[i].second), std::make_pair(10, 10),
-                std::make_pair(Tank::sandCamo, Tank::body),
-                std::make_pair(Tank::greenCamo, Tank::turret)));
+            std::make_shared<Tank>(
+                _settings._players.at(i).name, coords(_posTank[i].first, 0, _posTank[i].second),
+                std::make_pair(10, 10), std::make_pair(Tank::sandCamo, Tank::body), std::make_pair(Tank::greenCamo, Tank::turret)));
         auto tank = std::dynamic_pointer_cast<Tank>(_objects.back());
-        for (int y = 0; y != tank->getLife(); y++) {
-            _objects.emplace_back(std::make_shared<LifeGame>(tank->getName(),
-                coords(_uiLifePosPlayer[i].first + (30 * y),
-                    _uiLifePosPlayer[i].second)));
-        }
+        for (int y = 0; y != tank->getLife(); y++)
+            _objects.emplace_back(std::make_shared<LifeGame>(
+                tank->getName(), coords(_uiLifePosPlayer[i].first + (30 * y), _uiLifePosPlayer[i].second)));
+        setInputsFirstTank(_settings._keysPlayerOne);
         _objects.emplace_back(std::make_shared<TexteUI>(
             coords(_playerPos[i].first, _playerPos[i].second),
-            std::make_pair(50, 50), dynamic_cast<Tank &>( *tmp).getName(), 20,
+            std::make_pair(50, 50), dynamic_cast<Tank &>(*tmp).getName(), 20,
             1, std::make_pair(RGB(150), RGB())));
         _objects.emplace_back(std::make_shared<TexteUI>(
             coords(_scorePos[i].first, _scorePos[i].second),
             std::make_pair(50, 50),
-            std::to_string(dynamic_cast<Tank &>( *tmp).getScore()), 20, 1,
+            std::to_string(dynamic_cast<Tank &>(*tmp).getScore()), 20, 1,
             std::make_pair(RGB(150), RGB())));
     }
     auto const &colorPlayer = std::make_unique<ColorPlayer>();
-    for (auto &color : colorPlayer->getPosColorSquare()) {
+    for (auto &color : colorPlayer->getPosColorSquare())
+    {
         _objects.emplace_back(std::make_shared<UiObject>(color));
     }
     /////////////////////////////END CLEMENT//////////////////////:
@@ -70,33 +70,27 @@ SceneMaxime::SceneMaxime(Setting &settings) : AScene(settings), _pressed(false)
     std::vector<std::pair<int, int>> size;
 
     for (auto &i : _objects)
-        if (i->getTypeField().isTank) {
+        if (i->getTypeField().isTank)
             size.push_back(std::make_pair(static_cast<int>(i->getPosition().first), static_cast<int>(i->getPosition().third)));
-        }
     auto const &map = std::make_unique<Map>(size);
 
-    map->createDestructibleMap(std::make_pair(-8, -5), std::make_pair(1, 1));
-    map->createDestructibleMap(std::make_pair(-8, -1), std::make_pair(-1, -5));
-    map->createDestructibleMap(std::make_pair(1, -1), std::make_pair(5, -5));
-    map->createDestructibleMap(std::make_pair(1, 5), std::make_pair(5, 1));
+    map->createDestructibleMap(std::make_pair(-6, -7), std::make_pair(0, 0));
+    map->createDestructibleMap(std::make_pair(-6, 1), std::make_pair(-1, -7));
+    map->createDestructibleMap(std::make_pair(0, 0), std::make_pair(6, 7));
+    map->createDestructibleMap(std::make_pair(0, -7), std::make_pair(6, 0));
 
-    //map->createDestructibleMap(std::make_pair(1, 5), std::make_pair(5, 1));
     map->createContourMap(std::make_pair(-10, 10), std::make_pair(-8, 8));
 
-    setInputFunction(Raylib::ENTER, [&]() {
-        _enter = !_enter;
-    });
-
-    setInputFunction(Raylib::SPACE, [&]() {
-        _pressed = true;
-    });
+    setInputFunction(Raylib::ENTER, [&]()
+                     { _enter = !_enter; });
+    setInputFunction(Raylib::SPACE, [&]()
+                     { _pressed = true; });
     for (auto const &block : map->_objectNoDestructibleList)
         _objects.emplace_back(std::make_shared<Wall>(block));
-    for (auto const &block : map->_objectDestructibleList) {
+    for (auto const &block : map->_objectDestructibleList)
         _objects.emplace_back(std::make_shared<DestructibleWall>(block));
-    }
 
-/////////////////////////////END MAXIME//////////////////////:
+    /////////////////////////////END MAXIME//////////////////////:
 }
 
 SceneMaxime::~SceneMaxime()
@@ -109,13 +103,18 @@ void SceneMaxime::manageHeart(const std::string &name, const int life)
     std::size_t idx = 0;
     std::vector<int> tmp;
 
-    for (unsigned int i = 0; i != _objects.size(); i++) {
-        if (_objects[i]->getTypeField().isLife) {
+    for (unsigned int i = 0; i != _objects.size(); i++)
+    {
+        if (_objects[i]->getTypeField().isLife)
+        {
             auto heart = std::dynamic_pointer_cast<LifeGame>(_objects[i]);
-            if (heart->getName() == name) {
-                if (idx == life) {
+            if (heart->getName() == name)
+            {
+                if (idx == life)
+                {
                     _listPosHeart.push_back(i);
-                } else
+                }
+                else
                     ++idx;
             }
         }
@@ -124,8 +123,10 @@ void SceneMaxime::manageHeart(const std::string &name, const int life)
 
 void SceneMaxime::checkHeart() noexcept
 {
-    if (_listPosHeart.size() != 0) {
-        for (auto &i : _listPosHeart) {
+    if (_listPosHeart.size() != 0)
+    {
+        for (auto &i : _listPosHeart)
+        {
             _objects.erase(_objects.begin() + i);
         }
         _listPosHeart.clear();
@@ -136,31 +137,40 @@ Scenes SceneMaxime::run(Raylib &lib, Scenes const &prevScene)
 {
     bool isLock = false;
 
-    while (lib.gameLoop()) {
+    while (lib.gameLoop())
+    {
         triggerInputActions(lib);
         lib.printObjects(_objects);
-        if (_pressed && !isLock) {
+        if (_pressed && !isLock)
+        {
             _pressed = false;
             isLock = true;
-            for (auto it = _objects.begin(); it != _objects.end(); ) {
-                if (it->get()->getTypeField().isDestructible == true) {
-                    _objects.emplace_back(std::make_shared<PowerUps>(coords(it->get()->getPosition().first,it->get()->getPosition().second + 1.0f, it->get()->getPosition().third), std::make_pair(0, 0), std::pair<std::string, std::string>("", _assetsPath.at(2))));
-                    _objects.emplace_back(std::make_shared<Particles>(coords(it->get()->getPosition().first,it->get()->getPosition().second + 1.0f, it->get()->getPosition().third), std::make_pair(1, 1), 1.0f, 0.05f, std::make_pair(RGB(218, 165, 32), RGB()), 10, coords(0, 0.2f, 0)));
+            for (auto it = _objects.begin(); it != _objects.end();)
+            {
+                if (it->get()->getTypeField().isDestructible == true)
+                {
+                    _objects.emplace_back(std::make_shared<PowerUps>(coords(it->get()->getPosition().first, it->get()->getPosition().second + 1.0f, it->get()->getPosition().third), std::make_pair(0, 0), std::pair<std::string, std::string>("", _assetsPath.at(2))));
+                    _objects.emplace_back(std::make_shared<Particles>(coords(it->get()->getPosition().first, it->get()->getPosition().second + 1.0f, it->get()->getPosition().third), std::make_pair(1, 1), 1.0f, 0.05f, std::make_pair(RGB(218, 165, 32), RGB()), 10, coords(0, 0.2f, 0)));
                     it = _objects.erase(it);
                     break;
-                } else
+                }
+                else
                     ++it;
             }
         }
-        for (auto &it: _objects) {
-            if (it->getTypeField().isTank) {
+        for (auto &it : _objects)
+        {
+            if (it->getTypeField().isTank)
+            {
                 auto tank = std::dynamic_pointer_cast<Tank>(it);
                 manageHeart(tank->getName(), tank->getLife());
             }
-            if (it->getTypeField().isParticule == true) {
+            if (it->getTypeField().isParticule == true)
+            {
                 std::dynamic_pointer_cast<Particles>(it)->update();
             }
-            else if (it->getTypeField().isPowerUps == true) {
+            else if (it->getTypeField().isPowerUps == true)
+            {
                 std::dynamic_pointer_cast<PowerUps>(it)->rotate(0.5f);
             }
         }
