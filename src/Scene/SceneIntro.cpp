6867e38d41acadeb3fd087fa5e6f4c4886237263
Charlie CHOU <charlie.chou@epitech.eu>
2021-06-20 03:25:19 +0200

    fix: doom tank instead of our tank

--- src/Scene/SceneIntro.cpp
+++ src/Scene/SceneIntro.cpp
@@ -36,7 +36,7 @@ SceneIntro::SceneIntro(Setting &settings) : AScene(settings), _enter(false)
     _objects.emplace_back(std::make_shared<Tank>("tankLeft", coords(8,0, -7), coords(10, 10, 10), 8, std::make_pair(Tank::bodyTexture, Tank::bodyModel), std::make_pair(Tank::darkGreen, Tank::cannonModel)));
     std::dynamic_pointer_cast<Tank>(_objects.back())->rotateCannon(270);
 
-    _objects.emplace_back(std::make_shared<TexteUI>(coords(((settings._widthScreen / 2) - 50), ((settings._heightScreen / 4))), std::make_pair(100, 100), "Our Tank", 20, 1, std::make_pair(RGB(221, 131, 68), RGB())));
+    _objects.emplace_back(std::make_shared<TexteUI>(coords(((settings._widthScreen / 2) - 50), ((settings._heightScreen / 4))), std::make_pair(100, 100), "Doom Tank", 20, 1, std::make_pair(RGB(221, 131, 68), RGB())));
     _objects.emplace_back(std::make_shared<TexteUI>(coords(((settings._widthScreen / 2) - 110), ((settings._heightScreen / 1.1))), std::make_pair(50, 50), "press space to start", 20, 1, std::make_pair(RGB(177, 129, 78), RGB())));
     setInputFunction(Raylib::SPACE, [&]() {
         _enter = !_enter;
